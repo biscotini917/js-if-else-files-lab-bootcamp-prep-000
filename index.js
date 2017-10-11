8b037c015d589ca5b1e4eaeac3c50498e287e4f5
bruce leong <brucewleong@gmail.com>
2017-10-11 02:38:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var name = "Joe"
 var height = "74"
-
+if(name==="Susan") {
+  console.log("The name is correct")
+} else{
+  console.log("you're an idiot")
+}
 
 // Don't worry about this
 module.exports = { name, height
